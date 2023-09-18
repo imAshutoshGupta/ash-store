5c21b835ef289c266bc4dd343a9f78220fa40101
unknown <aguptaworkspace@gmail.com>
2023-09-18 15:52:55 +0530

    make migrations

--- ash_project/product_app/models.py
+++ ash_project/product_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+
+    name=models.CharField(max_length=50)
+    price=models.FloatField()
+    qty=models.IntegerField()
+    cat=models.IntegerField()
+    is_available=models.BooleanField()
