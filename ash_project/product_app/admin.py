cc546bd071500d6f2095e7bcceed93b62c4d07d3
unknown <aguptaworkspace@gmail.com>
2023-10-03 15:51:40 +0530

    done with model customizations and static files

--- ash_project/product_app/admin.py
+++ ash_project/product_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Product
 # Register your models here.
+
+#admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):           #for customization of the product table in column names
+    list_display=['name','price','qty','cat','is_available']        #empty list is defnined in ModelAdmin class, you just have to fill values
+    list_filter=('cat','is_available')      #to use filters in sorting #can be used as tuple also
+admin.site.register(Product,ProductAdmin)       #register in admin panel
+
+#dash UI customization
+admin.site.site_header="Ekart Dashboard"
+admin.site.site_title="Ekart"
--- ash_project/product_app/models.py
+++ ash_project/product_app/models.py
@@ -3,9 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
+    CAT=(
+        (1,'Mobile'),
+        (2,'Shoes'),
+        (3,'Clothes')
+    )       #used for making drop down choices and displaying the field
+    AVAIL=((1,"Yes"),(0,"No"))
 
-    name=models.CharField(max_length=50)
+    name=models.CharField(max_length=50,verbose_name='Product Name')
     price=models.FloatField()
-    qty=models.IntegerField()
-    cat=models.IntegerField()
-    is_available=models.BooleanField()
+    qty=models.IntegerField(verbose_name='Quantity')
+    cat=models.IntegerField(verbose_name='Category', choices=CAT)
+    is_available=models.BooleanField(verbose_name='Availability',choices=AVAIL)
+
+    def __str__(self):          #string representation of the object
+        #return str(self.id) #converting again to string as id is integer
+        return self.name
